cfc8c3ee73038e9c13fa729c5e65bf55992639c6
Zwe Lin Htet <zwelinhtet@Zwes-MacBook-Pro.local>
2019-09-14 12:08:31 +0630

    string

---
+++ string.py
@@ -0,0 +1,88 @@
+#Week 2A 14.9.2019
+x = "Hello World"
+print(x)
+print("'I can do everything Nothing impossible'")
+print('"I can do everything Nothing impossible"')
+
+>>> 5 * 'Hello' + 'World'
+'HelloHelloHelloHelloHelloWorld'
+>>> x= 'Hello' + 'World'
+>>> x
+'HelloWorld'
+>>> x='Hello' + ' ' + 'World'
+>>> x
+'Hello World'
+>>> "Hello" + str(3)
+'Hello3'
+>>> int("3")+3
+6
+
+>>> x = "Programming"
+>>> x[6]
+'m'
+>>> x[0]
+'P'
+>>> x[0:6]
+'Progra'
+>>> x[1:5]
+'rogr'
+>>> x[:7]
+'Program'
+>>> x[8:]
+'ing'
+>>> x[:]
+'Programming'
+>>> x[-4]
+'m'>>> x[-0]
+'P'
+>>> x[5:-4]
+'am'
+>>> x[-5:0]
+''
+>>> len(x)
+11
+>>> len(x) + 4
+15
+>>> len(x[5]) + 4
+5
+>>> len(x[5])
+1
+>>> len(x[5:9]) + 4
+8
+
+>>> x = 'Programming' #string	
+>>> word = [ 1, 3, 5, 7, 9, 11, 13, 15] #list
+>>> word
+[1, 3, 5, 7, 9, 11, 13, 15]
+>>> word[5]
+11
+>>> word[5:7]
+[11, 13]
+>>> word = [ 'P', 'r', 'o', 'g', 'r', 'a', 'm', 'm', 'i', 'n', 'g']
+>>> word[0] = "Q"
+>>> word
+['Q', 'r', 'o', 'g', 'r', 'a', 'm', 'm', 'i', 'n', 'g']
+>>> word[:] = "Good"
+>>> word
+['G', 'o', 'o', 'd']
+>>> word[2] 
+'o'
+
+>>> word[:] + [2, 4, 6, 8, 10]
+[1, 3, 5, 7, 9, 11, 13, 15, 2, 4, 6, 8, 10]
+>>> a = [ 1, 3, 5, 7, 9]
+>>> b = [ 2, 4, 6, 8, 10]
+>>> c = [ "A", "B", "C", "D", "E"]
+>>> x = a+ b + c
+>>> x
+[1, 3, 5, 7, 9, 2, 4, 6, 8, 10, 'A', 'B', 'C', 'D', 'E']
+>>> x =[a, b, c]
+>>> x
+[[1, 3, 5, 7, 9], [2, 4, 6, 8, 10], ['A', 'B', 'C', 'D', 'E']]
+>>> x[1:3]
+[[2, 4, 6, 8, 10], ['A', 'B', 'C', 'D', 'E']]
+>>> 
+
+
+
+
