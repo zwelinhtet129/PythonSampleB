e36c9ba74198fcd9df7e6d8331cac10d9dea9349
Zwe Lin Htet <zwelinhtet@Zwes-MacBook-Pro.local>
2019-09-21 12:17:35 +0630

    ifstatement

---
+++ ifstate.py
@@ -0,0 +1,54 @@
+#Week 3A 21.9.2019
+
+#x = int(input("Please enter an integer: "))
+#	x=0
+#	print("Negative changed to zero")
+#elif x==0:
+#	print("zero")
+#elif x==1:
+#	print("Single")
+#else:
+#	print("More")
+
+#x = int(input("Please enter an integer: "))
+#if 50 < x < 60:
+#	print("x is greater than 50")
+#elif x > 40:
+#	print("x is greater than 40")
+#elif x > 30:
+#	print("x is greater than 30")
+#else:
+#	print("More")
+
+#x = int(input("Please enter an integer: "))
+#if x >= 40 and x <= 70:
+#	print("Aged go to Bagan")
+#elif x < 40 and x >= 10:
+#	print("Youth go to Beach")
+#elif 0 < x < 10:
+#	print("Kid go to playground")
+#elif x > 70:
+#	print("unlisted")
+#else:
+#	print("Error")
+
+x = int (input("Enter mark = "))
+if x==100:
+	print("Scholar")
+elif x<100 and x >= 70:
+	print("Distinction")
+elif 70 > x >= 50:
+	print("Excellent")
+elif 50 > x >= 40:
+	print("Pass")
+elif 34<x<39:
+	print("Moderation")
+elif 40 > x and x > 10:
+	print("Fail") 
+elif x>=0 and x<=10:
+	print("Warning")
+else:
+	print("Invaid mark")
+
+
+
