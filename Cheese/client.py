a3dd380dc9a756ddd8c91f961cc43c7e123bd2f1
Zwe Lin Htet <zwelinhtet@Zwes-MacBook-Pro.local>
2019-10-05 12:16:31 +0630

    arguments list

---
+++ Cheese/client.py
@@ -0,0 +1,7 @@
+from cheeseshop import cheese
+
+cheeseshop("Limburger", "It's very runny, sir.",
+           "It's really very, VERY runny, sir.",
+           shopkeeper="Michael Palin",
+           client="John Cleese",
+           sketch="Cheese Shop Sketch")
--- fibonacci/fib1.py
+++ fibonacci/fib1.py
@@ -1,7 +1,17 @@
-import fib
+# import fib
 
-f = fib.fibo(500)
-print(f)
+# f = fib.fibo(500)
+# print(f)
 
-f = fib.fib2(500)
-print(f)
+# f = fib.fib2(500)
+# print(f)
+
+#from fib import fibo, fib2
+#f= fib.fib2(500)
+#print(f) 
+
+from fib import  *
+fib(500)
+
+from fibo import fib as fibonacci
+fibonacci(500)
