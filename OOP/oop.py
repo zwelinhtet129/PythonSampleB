8eeaf733682f2e035b05c74ba0d4be6fec54147d
Zwe Lin Htet <zwelinhtet@Zwes-MacBook-Pro.local>
2019-11-02 12:07:22 +0630

    OOP

--- OOP/oop.py
+++ OOP/oop.py
@@ -170,53 +170,176 @@
 #
 # Robot.how_many()
 
-class Dog:
+# class Dog:
+#
+#     population = 0
+#
+#     def __init__(self,name):
+#
+#         self.name = name
+#         print("Catch dog".format(self.name))
+#
+#         Dog.population +=  1
+#
+#     def die(self):
+#
+#         print("Dog {} is being fed.".format(self.name))
+#
+#         Dog.population -=  1
+#
+#         if Dog.population == 0:
+#             print("{} was the last one.".format(self.name))
+#         else:
+#             print("There are still {:d} dog waiting to be fed.".format(Dog.population))
+#     def say_hi(self):
+#             print("Dog {}.".format(self.name))
+#
+#     @classmethod
+#     def how_many(cls):
+#
+#             print("We have {:d} dogs.".format(cls.population))
+#
+# d1 = Dog("1")
+# d1.say_hi()
+# Dog.how_many()
+#
+# d2 = Dog("2")
+# d2.say_hi()
+# Dog.how_many()
+#
+# d3 = Dog('3')
+# d3.say_hi()
+# Dog.how_many()
+#
+# print("\nDogs are hungry.\n")
+#
+# print("let's feed and release them.")
+#
+# d1.die()
+# d2.die()
+# d3.die()
+#
+# Dog.how_many()
 
-    population = 0
 
-    def __init__(self,name):
+# class SchoolMember:
+#
+#     def __init__(self, name, age):
+#         self.name = name
+#         self.age = age
+#         print('(Initialized SchoolMember: {})'.format(self.name))
+#
+#     def tell(self):
+#
+#         print('Name: " {} " Age: " {} " '.format(self.name, self.age),end="")
+#
+# class Teacher (SchoolMember):
+#
+#     def __init__(self, name, age, marks):
+#         SchoolMember.__init__(self, name, age)
+#         self.marks = marks
+#         print('(Initialized Student:{})'.format(self.name))
+#
+#     def tell(self):
+#         SchoolMember.tell(self)
+#         print('Marks: "{:d}"'.format(self.marks))
+#
+#
+# class Student(SchoolMember):
+#
+#     def __init__(self, name, age, marks):
+#         SchoolMember.__init__(self, name, age)
+#         self.marks = marks
+#         print('(Initialized Student:{})'.format(self.name))
+#
+#     def tell(self):
+#         SchoolMember.tell(self)
+#         print('Marks: "{:d}"'.format(self.marks))
+#
+#
+#
+# class Teacher(SchoolMember):
+#
+#     def __init__(self, name, age, marks):
+#         SchoolMember.__init__(self, name, age)
+#         self.marks = marks
+#         print('(Initialized Student:{})'.format(self.name))
+#
+#     def tell(self):
+#         SchoolMember.tell(self)
+#         print('Marks: "{:d}"'.format(self.marks))
+#
+# class HeadMaster(SchoolMember):
+#
+#     def __init__(self, name, age, marks):
+#         SchoolMember.__init__(self, name, age)
+#         self.marks = marks
+#         print('(Initialized Student:{})'.format(self.name))
+#
+#     def tell(self):
+#         SchoolMember.tell(self)
+#         print('Marks: "{:d}"'.format(self.marks))
+#
+# t = Teacher('Mrs. Shrividya', 40, 30000)
+# s = Student('Swaroop', 25, 75)
+# h = HeadMaster('Mr. Tititata', 55, 100000)
+#
+# print()
+# members = [t, s, h]
+# for member in members:
+#     member.tell()
 
+class dog():
+    def __init__(self, name, age, sex):
         self.name = name
-        print("Catch dog".format(self.name))
+        self.age = age
+        self.sex = sex
+        print('(Initialized Dog: {})'.format(self.name))
 
-        Dog.population +=  1
+    def bark(self):
 
-    def die(self):
+         print('Name: " {} " Age: " {} " Sex: " {} '.format(self.name, self.age, self.sex),end="")
 
-        print("Dog {} is being fed.".format(self.name))
+class Shenoyed(dog):
 
-        Dog.population -=  1
+    def __init__(self, name, age, sex, puppy):
+        dog.__init__(self, name, age, sex)
+        self.puppy = puppy
+        print('(Initialized dog:{})'.format(self.name))
 
-        if Dog.population == 0:
-            print("{} was the last one.".format(self.name))
-        else:
-            print("There are still {:d} dog waiting to be fed.".format(Dog.population))
-    def say_hi(self):
-            print("Dog {}.".format(self.name))
+    def bark(self):
+        dog.bark(self)
+        print('Puppy: "{:d}"'.format(self.puppy))
 
-    @classmethod
-    def how_many(cls):
 
-            print("We have {:d} dogs.".format(cls.population))
+class Golden(dog):
 
-d1 = Dog("1")
-d1.say_hi()
-Dog.how_many()
+    def __init__(self, name, age, sex, puppy):
+        dog.__init__(self, name, age, sex)
+        self.puppy = puppy
+        print('(Initialized dog:{})'.format(self.name))
 
-d2 = Dog("2")
-d2.say_hi()
-Dog.how_many()
+    def bark(self):
+        dog.bark(self)
+        print('Puppy: "{:d}"'.format(self.puppy))
 
-d3 = Dog('3')
-d3.say_hi()
-Dog.how_many()
+class Huskey(dog):
 
-print("\nDogs are hungry.\n")
+    def __init__(self, name, age, sex, puppy, master):
+        dog.__init__(self, name, age, sex)
+        self.puppy = puppy
+        self.master = master
+        print('(Initialized dog:{})'.format(self.name))
 
-print("let's feed and release them.")
+    def bark(self):
+        dog.bark(self)
+        print('Puppy: "{}" Master: "{:d}" '.format(self.puppy, self.master))
 
-d1.die()
-d2.die()
-d3.die()
+s = Shenoyed('Bo Bo', '4', 'Female', 4)
+g = Golden('Shaung Shaung', '3', 'Male', 1)
+h = Huskey('Ki ki', '6', 'Female', 2, 2)
 
-Dog.how_many()
+print()
+members = [s, g, h]
+for member in members:
+    member.bark()
